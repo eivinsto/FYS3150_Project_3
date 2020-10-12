822e945fadccdf0028522a61386b54ec01449b01
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-10-12 20:22:18 +0200

    prototype file read and plot in python-script.

--- project.py
+++ project.py
@@ -5,18 +5,13 @@ from subprocess import run
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import os
 import shutil
 
 # retriveing working directories:
 rootdir = os.getcwd()
 src = rootdir + "/src"
-
-
-def main():
-    build_cpp()
-    test_cpp()
-    clean()
 
 
 def build_cpp():
@@ -30,6 +25,15 @@ def test_cpp():
     run("./test_main.exe", cwd=src)
 
 
+def read_cords(infile, numBods, colsPrBod):
+    xyz = np.zeros((numBods, 3))
+    line = infile.readline().split()
+    for i in range(numBods):
+        for j in range(colsPrBod):
+            xyz[i, j] = float(line[colsPrBod*i + j])
+    return xyz
+
+
 def clean(files="dat"):
     """Function for cleaning datafiles in src directory."""
     if files == "dat":
@@ -38,5 +42,45 @@ def clean(files="dat"):
         run(["make", "clean"], cwd=src)
 
 
-if __name__ == '__main__':
-    main()
+print("Set up run:")
+numTimesteps = 1000
+# dt = eval(input("Time step = "))
+filename = "positions.xyz"
+
+# Tstop = dt*numTimesteps
+
+build_cpp()
+run(["./main.exe", f"{numTimesteps}", filename], cwd=src)
+
+with open(rootdir + "/data/" + filename) as infile:
+    colsPrBod = 3
+    header1 = infile.readline().split()
+    numBods = int(header1[3][0])
+
+    for i in range(2):
+        infile.readline()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for i in range(numTimesteps):
+        xyz = read_cords(infile, numBods, colsPrBod)
+        if i > 0:
+            for j in range(numBods):
+                if j == 0:
+                    plottype = "b."
+                else:
+                    plottype = "-"
+
+                ax.plot([xyz[j, 0], old[j, 0]],
+                        [xyz[j, 1], old[j, 1]],
+                        [xyz[j, 2], old[j, 2]], plottype)
+
+        old = xyz.copy()
+
+plt.plot()
+plt.show()
+
+
+# test_cpp()
+clean()
