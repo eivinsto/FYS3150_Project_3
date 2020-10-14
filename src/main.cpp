a9eebd7d73387553891ec25a590da38395412b4a
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-10-14 17:19:13 +0200

    Adapted main and project for multiple different simulations

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,32 @@
 #include "solar_integrator.hpp"
 #include "solar_system.hpp"
 #include "celestial_body.hpp"
+#include <string>
 #include <armadillo>
 #include <cmath>
 
 using namespace std;
 
 int main(int numArguments, char **arguments) {
-  int numTimesteps = 1000;
-  if(numArguments >= 2) numTimesteps = atoi(arguments[1]);
+  std::string runflag = arguments[1];
+  if (runflag == "se") {
+    int numTimesteps = 1000;
+    if(numArguments >= 2) numTimesteps = atoi(arguments[2]);
 
-  // Reading initial state from file
-  SolarSystem solarSystem("../data/earth-sun-init.txt");
+    // Reading initial state from file
+    SolarSystem solarSystem("../data/earth-sun-init.txt");
+    solarSystem.moveToCOFMFrame();
 
-  double dt = 0.001;
-  if(numArguments >= 3) dt = atof(arguments[2]);
+    double dt = 0.001;
+    if(numArguments >= 3) dt = atof(arguments[3]);
 
-  solar_integrator integrator(dt, "VelocityVerlet");
-  solarSystem.initiateDataFile("../data/positions.xyz", "../data/energies.dat");
-  for(int timestep=0; timestep<numTimesteps; timestep++) {
-    integrator.integrateOneStep(solarSystem);
-    solarSystem.writeToFile();
-    solarSystem.writeEnergyToFile();
+    solar_integrator integrator(dt, "VelocityVerlet");
+    solarSystem.initiateDataFile("../data/positions.xyz", "../data/energies.dat");
+    for(int timestep=0; timestep<numTimesteps; timestep++) {
+      integrator.integrateOneStep(solarSystem);
+      solarSystem.writeToFile();
+      solarSystem.writeEnergyToFile();
+    }
   }
   return 0;
 }
